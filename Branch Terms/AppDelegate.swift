97a1e11a2f564ca86038ceb32d354823f6b8c5fb
Mike Horn <mhorn@branch.io>
2020-05-26 13:57:29 -0400

    first commit

---
+++ Branch Terms/AppDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.swift
+//  Branch Terms
+//
+//  Created by Michael Horn on 5/18/20.
+//  Copyright © 2020 Mike Horn. All rights reserved.
+//
+
+import UIKit
+import Branch
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+             // do stuff with deep link data (nav to page, display content, etc)
+            print(params as? [String: AnyObject] ?? {})
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil);
+                           let branchTermDetailViewController: BranchTermDetailViewController = storyboard.instantiateViewController(withIdentifier: "BranchTermDetailViewController") as! BranchTermDetailViewController;
+                           let quote = params?["quote"] as! String?
+                           if let unwrappedQuote = quote {
+                               branchTermDetailViewController.branchTermName = unwrappedQuote
+
+                           let rootViewController = self.window!.rootViewController as! UINavigationController;
+                           rootViewController.pushViewController(branchTermDetailViewController, animated: true);
+            }
+        
+        }
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return Branch.getInstance().application(app, open: url, options: options)
+    }
+
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+      // handler for Universal Links
+        return Branch.getInstance().continue(userActivity)
+    }
+
+}
+
